15b24f97791af9d65a9d7bb47899338bf2042376
Kennydamenni <736659@student.dusd.net>
2026-04-22 13:35:28 -0700

    finished
    8067 h789huie4wrtewQTR

--- Unit4WeimerP6/Assets/Scripts/SpawnManager.cs
+++ Unit4WeimerP6/Assets/Scripts/SpawnManager.cs
@@ -2,20 +2,39 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject powerupPrefab;
     public GameObject enemyPrefab;
     private float spawnRange = 9;
-    
+    public int enemyCount;
+    public int waveNumber = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i =0; i < enemiesToSpawn;  i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
